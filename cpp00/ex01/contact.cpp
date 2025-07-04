4415ee33f992ba27c312174804571070dd22e66d
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-07-04 15:51:41 +0300

    refactor: restructure repo

---
+++ cpp00/ex01/contact.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+class contact {
+	private:
+		std::string FirstName;
+		std::string LastName;
+		std::string NickName;
+		std::string PhoneNumber;
+		std::string DarkestSecret;
+	// public:
+		// make some kinf of constructor
+}
