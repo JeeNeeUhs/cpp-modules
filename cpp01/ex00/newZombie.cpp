28d33c1796b3eb7debd01868a503f1b7215028a1
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-07-22 22:52:00 +0300

    refactor: move Zombie destructor implementation to newZombie.cpp

--- cpp01/ex00/newZombie.cpp
+++ cpp01/ex00/newZombie.cpp
@@ -1,10 +1,6 @@
 #include "Zombie.hpp"
 #include <iostream>
 
-Zombie::~Zombie() {
-	std::cout << "Zombie named by " << name << " is destroyed." << std::endl;
-}
-
 Zombie* newZombie(const std::string& name) {
 	Zombie* zombie = new Zombie();
 	zombie->setName(name);
