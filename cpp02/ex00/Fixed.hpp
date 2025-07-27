f27856ad9020516c7e5b14d657a0e033f61e273b
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-07-27 19:35:23 +0300

    feat: implement Fixed class with constructors, destructor, and member functions

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,17 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed {
+	private:
+		int value;
+		static const int fractionalBits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		~Fixed();
+		Fixed &operator=(const Fixed &other);
+		int getRawBits() const;
+		void setRawBits(int raw);
+};
+
+#endif
