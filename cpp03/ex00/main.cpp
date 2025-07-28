4df514dbf9f08bf200cbeec8562bf3c32122b839
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-07-29 00:29:52 +0300

    refactor: simplify main function and enhance output formatting

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "ClapTrap.hpp"
 
 int main() {
@@ -6,20 +7,18 @@ int main() {
 	ClapTrap claptrap3(claptrap2);
 	ClapTrap claptrap4;
 	claptrap4 = claptrap1;
+	std::cout << "-------------------------------------------" << std::endl;
 
-	claptrap1.attack("target1");
-	claptrap1.takeDamage(3);
-	claptrap1.beRepaired(5);
-	
-	claptrap2.attack("target2");
-	claptrap2.takeDamage(3);
-	claptrap2.beRepaired(5);
-	
-	claptrap3.attack("target3");
-	claptrap3.takeDamage(5);
-	claptrap3.beRepaired(2);
-	
-	claptrap4.attack("target4");
-	claptrap4.takeDamage(10);
-	claptrap4.beRepaired(3);
+	claptrap1.attack("target");
+	claptrap2.takeDamage(1000);
+	claptrap3.beRepaired(50);
+	claptrap4.takeDamage(5);
+	std::cout << "-------------------------------------------" << std::endl;
+
+	for (int i = 0; i < 10; ++i) {
+		claptrap1.attack("targetloop");
+	}
+	std::cout << "-------------------------------------------" << std::endl;
+
+	return 0;
 }
