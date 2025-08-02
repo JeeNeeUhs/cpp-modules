2db7070aa18f4a7b87c5472826ff0da092829172
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-08-02 18:15:26 +0300

    feat: add WrongAnimal and WrongCat classes with constructors and sound methods

---
+++ cpp04/ex00/WrongCat.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() : WrongAnimal("WrongCat") {
+	std::cout << "WrongCat default constructor called" << std::endl;
+}
+
+WrongCat::WrongCat(const std::string &type) : WrongAnimal(type) {
+	std::cout << "WrongCat parameterized constructor called with type: " << type << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat &other) : WrongAnimal(other) {
+	std::cout << "WrongCat copy constructor called" << std::endl;
+}
+
+WrongCat &WrongCat::operator=(const WrongCat &other) {
+	if (this != &other) {
+		WrongAnimal::operator=(other);
+	}
+	return *this;
+}
+
+WrongCat::~WrongCat() {
+	std::cout << "WrongCat destructor called" << std::endl;
+}
+
+void WrongCat::makeSound() const {
+	std::cout << "Wrong meow meow" << std::endl;
+}
--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main() {
-	const Animal* dog = new Dog();
-	const Animal* cat = new Cat();
+	{
+		const Animal* animal = new Animal();
+		const Animal* dog = new Dog();
+		const Animal* cat = new Cat();
+		std::cout << "----------------------------------------------------------" << std::endl;
 
-	dog->makeSound();
-	cat->makeSound();
+		std::cout << animal->getType() << std::endl;
+		std::cout << dog->getType() << std::endl;
+		std::cout << cat->getType() << std::endl;
+		std::cout << "----------------------------------------------------------" << std::endl;
 
-	delete dog;
-	delete cat;
-	
+		animal->makeSound();
+		dog->makeSound();
+		cat->makeSound();
+		std::cout << "----------------------------------------------------------" << std::endl;
+
+		delete animal;
+		delete dog;
+		delete cat;
+		std::cout << "----------------------------------------------------------" << std::endl;
+	}
+	{
+		const WrongAnimal* wrongAnimal = new WrongAnimal();
+		const WrongAnimal* wrongCat = new WrongCat();
+		std::cout << "----------------------------------------------------------" << std::endl;
+
+		std::cout << wrongAnimal->getType() << std::endl;
+		std::cout << wrongCat->getType() << std::endl;
+		std::cout << "----------------------------------------------------------" << std::endl;
+
+		wrongAnimal->makeSound();
+		wrongCat->makeSound();
+		std::cout << "----------------------------------------------------------" << std::endl;
+
+		delete wrongAnimal;
+		delete wrongCat;
+	}
 	return 0;
 }
