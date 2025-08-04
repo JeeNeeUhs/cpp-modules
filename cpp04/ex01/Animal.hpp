5a3c5b5c7d16c8a04eab5c46d136e5690493dfd1
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-08-04 11:00:01 +0300

    feat: implement Animal, Cat, and Dog classes with constructors and sound methods

---
+++ cpp04/ex01/Animal.hpp
@@ -0,0 +1,22 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <string>
+
+class Animal {
+	protected:
+		std::string type;
+	public:
+		Animal();
+		Animal(const std::string& type);
+		Animal(const Animal& other);
+
+		virtual ~Animal();
+
+		Animal& operator=(const Animal& other);
+		
+		const std::string& getType() const;
+		virtual void makeSound() const;
+};
+
+#endif
