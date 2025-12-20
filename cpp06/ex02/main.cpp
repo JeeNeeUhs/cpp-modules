485f42c705e6c7350f7f91c64ad92fd7196f62ab
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2025-12-20 17:13:48 +0300

    feat: implement Base class and derived classes A, B, C with type identification functionality

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,76 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <ctime>
+#include <iostream>
+
+Base *generate(void)
+{
+	std::srand(time(0));
+	int rn = std::rand() % 3;
+
+	if (rn == 0)
+		return new A();
+	else if (rn == 1)
+		return new B();
+	else
+		return new C();
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown Type" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (std::bad_cast&) {}
+
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		return;
+	}
+	catch (std::bad_cast&) {}
+
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		return;
+	}
+	catch (std::bad_cast&) {}
+
+	std::cout << "Unknown Type" << std::endl;
+}
+
+int main(void)
+{
+	Base* basePtr = generate();
+
+	identify(basePtr);
+	identify(*basePtr);
+
+	delete basePtr;
+
+	Base baseObj;
+
+	identify(baseObj);
+	identify(baseObj);
+	return 0;
+}
